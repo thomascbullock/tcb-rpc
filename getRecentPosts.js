4bb841fed059a730d148cf2031a9dca147ba3826
Thom Bullock <t@thomascbullock.com>
2021-04-30 21:49:14 -0700

    a few fixes

--- getRecentPosts.js
+++ getRecentPosts.js
@@ -1,33 +1,40 @@
-const auth = require('./auth').auth;
-const authError = require('./auth').authError;
-const Postmaster = require('./postMaster');
-const path = require('path');
+const auth = require("./auth").auth;
+const authError = require("./auth").authError;
+const Postmaster = require("./postMaster");
+const path = require("path");
 
-exports.getRecentPosts = async function(params) {
-if (!auth(params)) {
-	return authError();
-}
+exports.getRecentPosts = async function (params) {
+  if (!auth(params)) {
+    return authError();
+  }
 
-const postmaster = new Postmaster();
-await postmaster.build('editor');
+  const postmaster = new Postmaster();
+  await postmaster.build("editor");
 
-const postArray = [];
-for (i = 0; i < postmaster.all.length; i++) {
-	const category = postmaster.all[i].type;
-	const categoryArr = [];
-	categoryArr.push(category);
-	const postId = `${postmaster.all[i].dateTime.replace(/-/g, '')}-${postmaster.all[i].slug}`;
-	const postToPush = {
-		dateCreated: postmaster.all[i].dateTime,
-		description: postmaster.all[i].body,
-		title: postmaster.all[i].title,
-		categories: categoryArr,
-		postid: postId,
-		link: path.join('https://thomascbullock.com/posts',postmaster.all[i].path,postmaster.all[i].slug),
-		slug: postmaster.all[i].slug
-	}
-	postArray.push(postToPush);
-}
+  const postArray = [];
+  for (i = 0; i < postmaster.all.length; i++) {
+    const category = postmaster.all[i].type;
+    const categoryArr = [];
+    categoryArr.push(category);
+    const postId = `${postmaster.all[i].dateTime.replace(/-/g, "")}-${
+      postmaster.all[i].slug
+    }`;
+    const postToPush = {
+      dateCreated: postmaster.all[i].dateTime,
+      description: postmaster.all[i].body,
+      title: postmaster.all[i].title,
+      categories: categoryArr,
+      postid: postId,
+      link: path.join(
+        "https://thomascbullock.com/posts",
+        postmaster.all[i].path,
+        postmaster.all[i].slug
+      ),
+      slug: postmaster.all[i].slug,
+      dateTime: postmaster.all[i].dateTime,
+    };
+    postArray.push(postToPush);
+  }
 
-return postArray;
-}
+  return postArray;
+};
